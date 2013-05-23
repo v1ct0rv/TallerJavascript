9ef25deb8dfc04235e525006eec9adf2d3c6d7b9
Pamela Rucinque <pamela.rucinque@gmail.com>
2013-05-23 17:13:33 -0500

    First chapter ready

--- 02-Introduccion-Javascript/Javascript101Examples.js
+++ 02-Introduccion-Javascript/Javascript101Examples.js
@@ -54,19 +54,66 @@ typeof(NaN); //Number
 isNaN(NaN); 
 
 /*************** Null vs Undefined ****************/
-window.noExiste; //Undefined
-var otraCosa;
-window.otraCosa; // Undefined
+window.nonExistent; //Undefined
+var aThing;
+window.aThing; // Undefined
 
-otraCosa = null; 
-window.otraCosa; 
+aThing = null; 
+window.aThing; 
 
 /*************** Falsy/Truthy Values **************/
 
-window.noExiste == window.otraCosa //true ?? --> Falsy Value
+window.nonExistent == window.aThing; //true ?? --> Falsy Value
 
 false + 1; 
 false == 0;
 
 true + 1;
 true == 1; 
+
+/*************** Operators **************/
+// + 
+3 + 4; 
+3 + ' Es un Número';
+
+// && 
+if(true && false){console.log('If this happens, I should never ever code again.')}
+if(true && true){console.log('This is expected.')}
+
+//--> It acts as a protector, avoids exceptions.
+var guardOperator = window.nonExistent.name; //throws exception
+var guardOperator = window.nonExistent && window.nonExistent.name; //no exception, sets Undefined
+
+window.nonExistent = {name: "Pam"};
+var guardOperator = window.nonExistent && window.nonExistent.name; //no exception, sets "Pam"
+
+// ||
+
+if(false || false){console.log('If this happens, I should never ever code again.')}
+if(true || false){console.log('This is expected.')}
+
+//--> It acts as a default value
+var defaultValue = window.noValue || "This is my default Value";
+
+window.aValue = "House";
+var defaultValue = window.aValue || "This is my default Value";
+
+
+/*************** Type Coercion  **************/
+
+1 == true;
+'0.1' == 0.1;
+'hola mundo' == ['hola mundo'];
+null == undefined;
+
+null == null;
+undefined == undefined;
+
+
+1 === true;
+'0.1' === 0.1;
+'hola mundo' === ['hola mundo'];
+null === undefined;
+
+null === null;
+undefined === undefined;
